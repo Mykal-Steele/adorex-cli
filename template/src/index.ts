89dbfa5c7aaadcd8db5e3f4e39d2240d111acd0f
Mykal-Steele <mykal.stele@gmail.com>
2026-03-16 22:24:16 +0700

    add logger to template

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
 import { prisma } from "./utils/prisma.js";
+import { logger } from "./utils/logger.js";
 
 const app = express();
 const port = parseInt(process.env.PORT || "3000", 10);
 
 app.use(express.json());
+app.use(logger);
 
 app.get("/", (_req, res) => {
   res.json({ message: "adorex app running" });
